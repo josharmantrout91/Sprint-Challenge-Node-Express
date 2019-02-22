3acffcc8c8c9bc68ee3a4b4aea8d1f6e31b745b8
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-22 12:39:38 -0500

    CREATE methods added for /api/projects/ and api/actions
    - attempting to introduce local middleware to check req.body.description.length for POST request to /api/actions

--- data/express-router.js
+++ data/express-router.js
@@ -1,11 +1,68 @@
 const express = require("express");
 
-const projectDb = require("./helpers/actionModel.js");
-const actionDb = require("./helpers/projectModel.js");
+const projectDb = require("./helpers/projectModel.js");
+const actionDb = require("./helpers/actionModel.js");
+
+function checkActionDescLength() {
+  return function(req, res, next) {
+    let actionDescLength = req.body.description.length;
+    if (actionDescLength <= 128) {
+      next();
+    } else {
+      res
+        .status(404)
+        .json(
+          "We aint tryna read a novel! Keep that description under 128 characters fool!!"
+        );
+    }
+  };
+}
 
 const router = express.Router();
 
 // ********** CREATE METHODS ********** //
+
+// POST request to /api/projects
+router.post("/projects", async (req, res) => {
+  try {
+    const project = await projectDb.insert(req.body);
+
+    if (req.body.name && req.body.description) {
+      res.status(201).json(project);
+    } else {
+      res.status(400).json({
+        errorMessage:
+          "Ay! You Crazy?!? This project NEEDS a NAME AND DESCRIPTION!!"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error:
+        "I dunno what to tell you bro... I just cant save that project for you"
+    });
+  }
+});
+
+// POST request to /api/actions
+router.post("/actions", async (req, res) => {
+  try {
+    const action = await actionDb.insert(req.body);
+
+    if (req.body.project_id && req.body.description && req.body.notes) {
+      res.status(201).json(action);
+    } else {
+      res.status(400).json({
+        errorMessage:
+          "Ay! You Crazy?!? This action NEEDS a PROJECT ID, DESCRIPTION, AND NOTES!!"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error:
+        "I dunno what to tell you bro... I just cant save that action for you"
+    });
+  }
+});
 
 // ********** READ METHODS ********** //
 
