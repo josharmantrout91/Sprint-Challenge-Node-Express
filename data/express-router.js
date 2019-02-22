aeb211368daf8de32a24d1e7dc41118a638955a8
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-22 11:38:17 -0500

    Server Listening and able to get root

---
+++ data/express-router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const projectDb = require("./helpers/actionModel.js");
+const actionDb = require("./helpers/projectModel.js");
+
+const router = express.Router();
+
+module.exports = router;
