79bed6f9a7cb78e1abf0b9ffa7370b07960eeacf
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-22 11:56:35 -0500

    GET methods functional for /api/projects and /api/actions. Created GET method for projectActions but method currently returning err status 500.

--- data/express-router.js
+++ data/express-router.js
@@ -5,4 +5,54 @@ const actionDb = require("./helpers/projectModel.js");
 
 const router = express.Router();
 
+// ********** CREATE METHODS ********** //
+
+// ********** READ METHODS ********** //
+
+// GET request to /api/projects
+router.get("/projects/", async (req, res) => {
+  try {
+    const projects = await projectDb.get();
+    res.status(200).json(projects);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Aint no projects here, yall");
+  }
+});
+
+// GET request to /api/actions
+router.get("/actions/", async (req, res) => {
+  try {
+    const actions = await actionDb.get();
+    res.status(200).json(actions);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Aint no actions here, yall");
+  }
+});
+
+// GET request to /api/projects/:id/actions
+router.get("/projects/:id/actions", async (req, res) => {
+  try {
+    const projectActions = await projectDb.getProjectActions(req.params.id);
+
+    if (projectActions) {
+      res.status(200).json(projectActions);
+    } else {
+      res.status(404).json({
+        error: "You must be crazy, that project doesnt have any actions!"
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "I dunno where those project actions are, but they aint here!"
+    });
+  }
+});
+
+// ********** UPDATE METHODS ********** //
+
+// ********** DELETE METHODS ********** //
+
 module.exports = router;
